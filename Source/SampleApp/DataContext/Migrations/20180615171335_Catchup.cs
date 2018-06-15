d5f20bdabec25abb07e2f1eff352b2678f198d1a
John Todd <todd@iticentral.com>
2018-06-15 13:16:31 -0400

    cleanup

---
+++ Source/SampleApp/DataContext/Migrations/20180615171335_Catchup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataContext.Migrations
+{
+    public partial class Catchup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "User",
+                table: "AuditEntries",
+                newName: "UserName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "AuditEntries",
+                maxLength: 64,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "AuditEntries");
+
+            migrationBuilder.RenameColumn(
+                name: "UserName",
+                table: "AuditEntries",
+                newName: "User");
+        }
+    }
+}
