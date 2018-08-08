45455655fa4736c2ec04a49fc1d81af14ce90a9e
John Todd <todd@iticentral.com>
2018-08-08 12:49:32 -0400

    Added VoiceMessage class

---
+++ Source/Baseline/Iti.Voice/IVoiceMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace Iti.Voice
+{
+    public interface IVoiceMessage
+    {
+        string ContentMarkup { get; }
+        void Say(string message, int? loop = null);
+        void Pause(int seconds);
+    }
+}
