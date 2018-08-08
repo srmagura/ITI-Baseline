b628043951a4e16481feff87784d49118c2830a6
John Todd <todd@iticentral.com>
2018-08-08 14:13:59 -0400

    Added no op url shortener

---
+++ Source/Baseline/Iti.Core/UrlShortener/NoOpUrlShortener.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Iti.Core.UrlShortener
+{
+    public class NoOpUrlShortener : IUrlShortener
+    {
+        public string Shorten(string url)
+        {
+            // no operation (NoOp)
+            return url;
+        }
+    }
+}
