9d712b9c94359c05fe577053bd35e45678fa6341
Sam Magura <srmagura@gmail.com>
2021-04-25 12:57:52 -0400

    add HashSet test

--- Source/Tests/UnitTests/Domain/IdentityTests.cs
+++ Source/Tests/UnitTests/Domain/IdentityTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using ITI.DDD.Domain.Entities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -27,6 +28,26 @@ namespace UnitTests.Domain
             Assert.IsFalse(id_null1 == id1);
 
             Assert.IsTrue(id_null1 == id_null2);
+        }
+
+        [TestMethod]
+        public void HashSet()
+        {
+            var id1 = new TestId();
+            var id2 = new TestId();
+            var id1_copy = new TestId(id1.Guid);
+
+            var set = new HashSet<TestId> { id1 };
+
+            Assert.IsTrue(set.Contains(id1));
+            Assert.IsTrue(set.Contains(id1_copy));
+
+            set.Add(id1);
+            set.Add(id1_copy);
+            Assert.AreEqual(1, set.Count);
+
+            set.Add(id2);
+            Assert.AreEqual(2, set.Count);
         }
     }
 
